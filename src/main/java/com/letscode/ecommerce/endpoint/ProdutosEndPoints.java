8c0b405f9e8bb1fe958f6fe250cafbb75e6e2bb1
f8214106 Pedro Antonio de Almeida Adelino <pedro.adelino@bb.com.br>
2022-08-31 17:53:34 -0300

    criado ProdutosEndPoints

---
+++ src/main/java/com/letscode/ecommerce/endpoint/ProdutosEndPoints.java
@@ -0,0 +1,32 @@
+package com.letscode.ecommerce.endpoint;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProdutosEndPoints {
+
+    @RequestMapping(path = "/novoProduto", method = RequestMethod.POST)
+    public ResponseEntity<String> novoProduto() {
+        return new ResponseEntity<String>("Produto criado com sucesso", HttpStatus.CREATED);
+    }
+
+    @RequestMapping(path = "/pegaProduto/{id}", method = RequestMethod.GET)
+    public ResponseEntity<String> pegaProdutoPorId(@PathVariable String id) {
+        return new ResponseEntity<String>("Requisitado produto id: " + id, HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/excluirProduto/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<String> excluirProdutoPorId(@PathVariable String id) {
+        return new ResponseEntity<String>("Requisitada exclusão do produto id: " + id, HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "/atualizarProduto/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<String> atualizarProdutoPorId(@PathVariable String id) {
+        return new ResponseEntity<String>("Requisitada atualização do produto id: " + id, HttpStatus.OK);
+    }
+}
