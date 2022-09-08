3401ea17ed0f18af60254e138e24db62b315a4e7
f8214106 Pedro Antonio de Almeida Adelino <pedro.adelino@bb.com.br>
2022-09-08 14:30:58 -0300

    atualizado ClienteDao, ClienteDTO e ClienteServiceImpl

--- src/main/java/com/letscode/ecommerce/dto/ClienteDto.java
+++ src/main/java/com/letscode/ecommerce/dto/ClienteDto.java
@@ -1,5 +1,10 @@
 package com.letscode.ecommerce.dto;
 
+import com.letscode.ecommerce.models.PerfilEnum;
+
+import lombok.Data;
+
+@Data
 public class ClienteDto {
 
     private long id;
@@ -14,8 +19,11 @@ public class ClienteDto {
 
     private String cpf;
 
-    public ClienteDto(long id, String nome, String sobrenome, String email, String sexo, String cpf) {
-        this.id = id;
+    private String senha;
+
+    private PerfilEnum perfil;
+
+    public ClienteDto(String nome, String sobrenome, String email, String sexo, String cpf) {
         this.nome = nome;
         this.sobrenome = sobrenome;
         this.email = email;
@@ -23,51 +31,6 @@ public class ClienteDto {
         this.cpf = cpf;
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getSobrenome() {
-        return sobrenome;
-    }
-
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSexo() {
-        return sexo;
-    }
-
-    public void setSexo(String sexo) {
-        this.sexo = sexo;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
+    public ClienteDto() {
+    };
 }
--- src/main/java/com/letscode/ecommerce/service/impl/ClienteServiceImpl.java
+++ src/main/java/com/letscode/ecommerce/service/impl/ClienteServiceImpl.java
@@ -3,6 +3,7 @@ package com.letscode.ecommerce.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.letscode.ecommerce.dao.ClienteDao;
@@ -16,27 +17,51 @@ public class ClienteServiceImpl implements ClienteService {
     @Autowired
     ClienteDao clienteDao;
 
+    private final PasswordEncoder passwordEncoder;
+
+    public ClienteServiceImpl(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     public List<Cliente> listarTodosClientes() {
         return clienteDao.findAll();
     }
 
     @Override
-    public Cliente novoCliente(ClienteDto cliente) {
-        // TODO Auto-generated method stub
-        return null;
+    public Cliente novoCliente(ClienteDto clienteDto) {
+        try {
+            if (clienteDto.getId() == 0) {
+                clienteDto.setSenha(passwordEncoder.encode(clienteDto.getSenha()));
+            }
+            Cliente cliente = new Cliente(clienteDto.getNome(), clienteDto.getSobrenome(), clienteDto.getEmail(),
+                    clienteDto.getSexo(), clienteDto.getCpf(), clienteDto.getSenha(), clienteDto.getPerfil());
+            clienteDao.save(cliente);
+            cliente.setSenha("");
+            return cliente;
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     @Override
     public boolean atualizarCliente(Cliente cliente) {
-        // TODO Auto-generated method stub
-        return false;
+        try {
+            clienteDao.save(cliente);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
     @Override
     public boolean removerCliente(long id) {
-        // TODO Auto-generated method stub
-        return false;
+        try {
+            clienteDao.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 
 }
