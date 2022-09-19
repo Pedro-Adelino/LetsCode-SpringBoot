75f8e3dbee7cf70ae83ce74d13ae109df3b1dc01
f8214106 Pedro Antonio de Almeida Adelino <pedro.adelino@bb.com.br>
2022-09-19 11:27:22 -0300

    Adicionada classe Pedidos ao package modelos

---
+++ src/main/java/com/letscode/ecommerce/models/Pedido.java
@@ -0,0 +1,28 @@
+package com.letscode.ecommerce.models;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "pedidos")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "cliente")
+    private Cliente cliente;
+
+    @Column(name = "produtos")
+    private List<Produto> produtos;
+}
